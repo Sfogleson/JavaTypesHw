04dc09211fbecd6b2ab6b5727ae1965d2701dc60
Sfogleson <77253297+Sfogleson@users.noreply.github.com>
2021-02-13 12:04:39 -0600

    First Commit

---
+++ JavaTypesHw/src/javaTypes/Main.java
@@ -0,0 +1,16 @@
+package javaTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String firstName = null;
+		System.out.println(firstName);
+		
+		
+		//String firstName = new String();
+		//firstName = "blah";
+		//System.out.println(firstName);
+		
+	}
+
+}
